bdf67c3c39db81e309f6ce77bbd02308dc261659
Ilya Maltsev <145224635+noble-comrade@users.noreply.github.com>
2024-10-25 16:27:36 +0300

    Add files via upload

---
+++ someProject123/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите верхний(a) лимит>>");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите нижний(b) лимит>>");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите количество разбиений на прямоугольники>>");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string path = "C:\\Users\\prdb\\Desktop\\someProject123\\answer.txt";
+double h = (b - a)/n;
+double result = 0;
+
+double fun(double x) 
+{
+    return 2*Math.Pow(x, 2) + 3*x;
+}
+
+for (double i = 1; i <= n; i++) 
+{
+    double sigma = a + (i * h) / 2;
+    result += fun(sigma);
+}
+result *= h;
+
+Console.WriteLine($"Ответ был записан в {path}");
+using (StreamWriter writer = new StreamWriter(path, false)) 
+{
+    await writer.WriteLineAsync(Convert.ToString(result));
+}
